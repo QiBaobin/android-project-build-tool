6972f96c6295aa88483aa3ff4fc4893a0578d1af
Bob Qi <Baobin.Qi@gmail.com>
2023-10-13 09:00:24 +0800

    move regex_t to c file from h file

--- lib/regez/regez.h
+++ lib/regez/regez.h
@@ -1,12 +1,3 @@
-#include <regex.h>
+int compile(char const *pattern);
 
-extern regex_t re;
-
-int compile(char const *pattern) {
-  return regcomp(&re, pattern, REG_EXTENDED);
-}
-
-int isMatch(char const *input) {
-  regmatch_t pmatch[0];
-  return regexec(&re, input, 0, pmatch, 0);
-}
+int isMatch(char const *input);
--- src/regez.c
+++ src/regez.c
@@ -1,3 +1,12 @@
 #include <regex.h>
 
 regex_t re;
+
+int compile(char const *pattern) {
+  return regcomp(&re, pattern, REG_EXTENDED);
+}
+
+int isMatch(char const *input) {
+  regmatch_t pmatch[0];
+  return regexec(&re, input, 0, pmatch, 0);
+}
